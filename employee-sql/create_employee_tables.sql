db5b1a0bdd85d87cddee1c122629ac05b87a8f91
siobyrne <siobyrne@users.noreply.github.com>
2023-08-22 09:01:56 -0400

    Refine employee tables

--- employee-sql/create_employee_tables.sql
+++ employee-sql/create_employee_tables.sql
@@ -1,21 +1,21 @@
 CREATE TABLE departments(
-	dept_no VARCHAR(100) PRIMARY KEY,
-	dept_name VARCHAR(100)
+	dept_no VARCHAR(10) PRIMARY KEY,
+	dept_name VARCHAR(50)
 );
 
 CREATE TABLE titles(
-	title_id VARCHAR(100) PRIMARY KEY,
-	title VARCHAR(100)
+	title_id VARCHAR(10) PRIMARY KEY,
+	title VARCHAR(50)
 );
 
 CREATE TABLE employees(
 	emp_no INT PRIMARY KEY,
-	emp_title_id VARCHAR (100),
-	birth_date VARCHAR(100),
-	first_name VARCHAR(100),
-	last_name VARCHAR(100),
+	emp_title_id VARCHAR (10),
+	birth_date VARCHAR(15),
+	first_name VARCHAR(25),
+	last_name VARCHAR(25),
 	sex VARCHAR(5),
-	hire_date VARCHAR(100),
+	hire_date VARCHAR(15),
 	CONSTRAINT fk_emp_title
 		FOREIGN KEY(emp_title_id)
 			REFERENCES titles(title_id)
@@ -31,12 +31,12 @@ CREATE TABLE salaries(
 
 CREATE TABLE dept_emp(
 	emp_no INT,
-	dept_no VARCHAR(100),
+	dept_no VARCHAR(10),
 	PRIMARY KEY(emp_no, dept_no)
 );
 
 CREATE TABLE dept_manager(
-	dept_no VARCHAR(100),
+	dept_no VARCHAR(10),
 	emp_no INT,
 	PRIMARY KEY(dept_no, emp_no)
 );
